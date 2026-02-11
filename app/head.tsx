c68933c43b10ce27249cc7308c457a6907557166
Mathieu Marciniak <mathieu@email.com>
2026-02-11 15:10:44 -0500

    phrase pour reconnaitre site impact affiliation

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta name="impact-site-verification" content="b3e59d2b-b9c5-4a0d-bcf3-caccc26c52a4" />
+    </>
+  );
+}
