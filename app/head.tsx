e07b7e996e96166cfe8b23b5f7be3e8182c30fb0
Mathieu Marciniak <mathieu@email.com>
2026-02-13 12:41:53 -0500

    Add Impact verification meta

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,12 @@
 export default function Head() {
   return (
     <>
+      {/* standard */}
       <meta name="impact-site-verification" content="b3e59d2b-b9c5-4a0d-bcf3-caccc26c52a4" />
+
+      {/* version EXACTE demandée par Impact (on force l'attribut value) */}
+      {/* @ts-expect-error Impact expects `value` attribute */}
+      <meta name="impact-site-verification" value="b3e59d2b-b9c5-4a0d-bcf3-caccc26c52a4" />
     </>
   );
 }
